550e39a8d19c489587a17f04024b794ae5d257ae
Kayode Olaniyi <kaysolaniyi@gmail.com>
2019-02-12 17:58:05 +0100

    fixed issue with default factor

--- database/migrations/2019_02_12_095523_edit_records_table.php
+++ database/migrations/2019_02_12_095523_edit_records_table.php
@@ -15,8 +15,8 @@ class EditRecordsTable extends Migration
     {
         //
         Schema::table('records',function(Blueprint $table){
-            $table->boolean('disabled');
-            $table->boolean('enabled');
+            $table->boolean('disabled')->default(false);
+            $table->boolean('enabled')->default(false);
         });
     }
 
