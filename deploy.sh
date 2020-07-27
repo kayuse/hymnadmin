c5159fa9bf158d5b0518bd653aa974a4db4f3944
Kayode Olaniyi <kaysolaniyi@gmail.com>
2020-07-27 11:21:56 +0100

    idea

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 git pull
 composer install
 php artisan migrate
-php artisan config:cache
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 sudo service nginx restart
+
 
