e5ef8ac3b8ed6906e83d26ae6dc77301c7469e03
Kayode Olaniyi <kaysolaniyi@gmail.com>
2020-12-27 16:02:07 +0100

    made changes to frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,52 +61,45 @@
                 <div class="features-inner section-inner has-bottom-divider">
                     <div class="features-header text-center">
                         <div class="container-sm">
-                            <h2 class="section-title mt-0">The Product</h2>
-                            <p class="section-paragraph">Lorem ipsum is common placeholder text used to demonstrate the graphic elements of a document or visual presentation.</p>
-                            <div class="features-image">
-                                <img class="features-illustration asset-dark" src="dist/images/features-illustration-dark.svg" alt="Feature illustration">
-                                <img class="features-box asset-dark" src="dist/images/features-box-dark.svg" alt="Feature box">
-                                <img class="features-illustration asset-dark" src="dist/images/features-illustration-top-dark.svg" alt="Feature illustration top">
-                                <img class="features-illustration asset-light" src="dist/images/features-illustration-light.svg" alt="Feature illustration">
-                                <img class="features-box asset-light" src="dist/images/features-box-light.svg" alt="Feature box">
-                                <img class="features-illustration asset-light" src="dist/images/features-illustration-top-light.svg" alt="Feature illustration top">
-                            </div>
+                            <h2 class="section-title mt-0">What we are about</h2>
+                            <p class="section-paragraph">We believe we can bless lives by reaching out to this dying world with the gospel of Jesus.</p>
+
                         </div>
                     </div>
                     <div class="features-wrap">
                         <div class="feature is-revealing">
                             <div class="feature-inner">
                                 <div class="feature-icon">
-                                    <img class="asset-light" src="dist/images/feature-01-light.svg" alt="Feature 01">
-                                    <img class="asset-dark" src="dist/images/feature-01-dark.svg" alt="Feature 01">
+                                    <img class="asset-light" src="{{asset('landing/images/feature-01-light.svg')}}" alt="Feature 01">
+                                    <img class="asset-dark" src="{{asset('landing/images/feature-01-dark.svg')}}" alt="Feature 01">
                                 </div>
                                 <div class="feature-content">
-                                    <h3 class="feature-title mt-0">Discover</h3>
-                                    <p class="text-sm mb-0">Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua dui.</p>
+                                    <h3 class="feature-title mt-0">Podcasts</h3>
+                                    <p class="text-sm mb-0">Bless the Church of Christ with an amazing podcast</p>
                                 </div>
                             </div>
                         </div>
                         <div class="feature is-revealing">
                             <div class="feature-inner">
                                 <div class="feature-icon">
-                                    <img class="asset-light" src="dist/images/feature-02-light.svg" alt="Feature 02">
-                                    <img class="asset-dark" src="dist/images/feature-02-dark.svg" alt="Feature 02">
+                                    <img class="asset-light" src="{{asset('landing/images/feature-02-light.svg')}}" alt="Feature 02">
+                                    <img class="asset-dark" src="{{asset('landing/images/feature-02-dark.svg')}}" alt="Feature 02">
                                 </div>
                                 <div class="feature-content">
-                                    <h3 class="feature-title mt-0">Discover</h3>
-                                    <p class="text-sm mb-0">Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua dui.</p>
+                                    <h3 class="feature-title mt-0">Songs</h3>
+                                    <p class="text-sm mb-0">Bless the Church with spirit inspired songs</p>
                                 </div>
                             </div>
                         </div>
                         <div class="feature is-revealing">
                             <div class="feature-inner">
                                 <div class="feature-icon">
-                                    <img class="asset-light" src="dist/images/feature-03-light.svg" alt="Feature 03">
-                                    <img class="asset-dark" src="dist/images/feature-03-dark.svg" alt="Feature 03">
+                                    <img class="asset-light" src="{{asset('landing/images/feature-03-light.svg')}}" alt="Feature 03">
+                                    <img class="asset-dark" src="{{asset('landing/images/feature-03-dark.svg')}}" alt="Feature 03">
                                 </div>
                                 <div class="feature-content">
-                                    <h3 class="feature-title mt-0">Discover</h3>
-                                    <p class="text-sm mb-0">Lorem ipsum dolor sit amet, consecte adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua dui.</p>
+                                    <h3 class="feature-title mt-0">Prayers</h3>
+                                    <p class="text-sm mb-0">Lead people to reach to God via prayer</p>
                                 </div>
                             </div>
                         </div>
@@ -119,10 +112,10 @@
             <div class="container-sm">
                 <div class="cta-inner section-inner">
                     <div class="cta-header text-center">
-                        <h2 class="section-title mt-0">Get it and Switch</h2>
-                        <p class="section-paragraph">Lorem ipsum is common placeholder text used to demonstrate the graphic elements of a document or visual presentation.</p>
+                        <h2 class="section-title mt-0">Submit your God inspiring content and we shall air it for you</h2>
+                        <p class="section-paragraph">Awesome, let's get started</p>
                         <div class="cta-cta">
-                            <a class="button button-primary" href="#">Buy it now</a>
+                            <a class="button button-primary" href="#">Get Started</a>
                         </div>
                     </div>
                 </div>
@@ -135,8 +128,8 @@
             <div class="site-footer-inner">
                 <div class="brand footer-brand">
                     <a href="#">
-                        <img class="asset-light" src="dist/images/logo-light.svg" alt="Logo">
-                        <img class="asset-dark" src="dist/images/logo-dark.svg" alt="Logo">
+                        <img class="asset-light" src="{{asset('landing/images/logo-light.svg')}}" alt="Logo">
+                        <img class="asset-dark" src="{{asset('landing/images/logo-dark.svg')}}" alt="Logo">
                     </a>
                 </div>
                 <ul class="footer-links list-reset">
@@ -179,7 +172,7 @@
                         </a>
                     </li>
                 </ul>
-                <div class="footer-copyright">&copy; 2018 Switch, all rights reserved</div>
+                <div class="footer-copyright">&copy; {{date('Y')}} Ilanaa, all rights reserved</div>
             </div>
         </div>
     </footer>
